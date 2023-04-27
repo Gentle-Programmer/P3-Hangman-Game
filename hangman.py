d042aecc7d9d54a95fd6f0ce863bb9585d183394
Gentle-Programmer <annatataruch@gmail.com>
2023-04-27 08:48:17 +0000

    Add question for starting the game

--- hangman.py
+++ hangman.py
@@ -9,5 +9,7 @@ def dispaly_welcome():
 ========='''"""
     print(logo)
     print("Welcome to Hangman")
+    start_game = input("Do you want to start the game? (y/n): ").lower()
+    return start_game == "y"
 
 dispaly_welcome()
