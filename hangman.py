5774f8932a2ac569983c81426a213cfce74652d7
Gentle-Programmer <annatataruch@gmail.com>
2023-04-27 08:43:20 +0000

    Create welcome function with logo graf

---
+++ hangman.py
@@ -0,0 +1,13 @@
+def dispaly_welcome():
+    logo = r"""
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+========='''"""
+    print(logo)
+    print("Welcome to Hangman")
+
+dispaly_welcome()
