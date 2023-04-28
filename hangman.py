3601261e797a0a380bf5ae377def586cb53e1e63
Gentle-Programmer <annatataruch@gmail.com>
2023-04-28 08:16:00 +0000

    Change welcome function with imported logo

--- hangman.py
+++ hangman.py
@@ -1,24 +1,10 @@
 import random
-form hangman_logo import HANGMAN_LOGO, HANGMAN_STAGES
+from hangman_logo import HANGMAN_LOGO, HANGMAN_STAGES
 
 def dispaly_welcome():
-    logo = r"""
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-========='''"""
-    print(logo)
-    print("Welcome to Hangman")
-    start_game = input("Do you want to start the game? (y/n): ").lower()
-    return start_game == "y"
+    print(HANGMAN_LOGO)
+    print("Welcome to Hangman!")
 
-if dispaly_welcome():
-    print("Let's get started")
-else:
-    print("Goodbye!")
 
 def get_name():
     return input("Enter your name: ")
@@ -31,61 +17,9 @@ def get_user_guess():
     return input("Enter a letter: ").lower()
 
 def display_game_status():
-    hangman_parts = [
-       r"""
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========''', r"""
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========''', r"""
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========''', r"""
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', r"""
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========''', r"""
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========''', r"""
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========''']
+  pass
 
     
 def main_game_loop():
-    name = get_player_name()
-    word = select_random_word()
-
-main_game_loop()
+    pass
+  
